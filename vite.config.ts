7be69cbf03b33f29060702cc31afc23bc71158bb
Daniele Araújo <s.araujodaniele@gmail.com>
2026-04-03 13:54:57 -0300

    feat: add GitHub Actions workflow for deployment and configure Vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
   plugins: [react()],
   resolve: {
